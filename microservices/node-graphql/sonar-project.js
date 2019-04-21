7b413e803e25d6eb17ae2044cab4017146e89d80
venkypuppala <48930436+venkypuppala@users.noreply.github.com>
2019-04-21 16:33:18 -0700

    Update sonar-project.js

--- microservices/node-graphql/sonar-project.js
+++ microservices/node-graphql/sonar-project.js
@@ -4,7 +4,7 @@ sonarqubeScanner({
   options : {
   'sonar.sources': '.',
   'sonar.inclusions': './**',
-  'sonar.tests': '/home/jenkins/workspace/microservices-demo/microservices/node-graphql/test/*',
+  'sonar.tests': './test/**',
   'sonar.exclusions': 'node_modules/*,coverage/*',
   'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
   'sonar.language': 'js',
