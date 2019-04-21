c9f5c1ba0a00924dc2624b8f9fc1442ab9a27ed6
venkypuppala <48930436+venkypuppala@users.noreply.github.com>
2019-04-21 15:34:28 -0700

    Add files via upload

---
+++ microservices/node-graphql/sonar-project.js
@@ -0,0 +1,15 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+sonarqubeScanner({
+  serverUrl: 'http://104.196.201.144:9000',
+  options : {
+  'sonar.sources': '.',
+  'sonar.inclusions': 'packages/core/src/**',
+  'sonar.tests': './test/*',
+  'sonar.exclusions': './node_modules/*,./coverage/*',
+  'sonar.javascript.lcov.reportPaths': './coverage/lcov.info',
+  'sonar.language': 'js',
+  'sonar.sourceEncoding': 'UTF-8',
+  'sonar.projectKey': 'node-graphql',
+  'sonar.projectVersion': '1.0'
+  }
+}, () => {});
